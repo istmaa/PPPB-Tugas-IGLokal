c67915912f979fed8112c121fe900600f80f2e0b
istmaa <ulhaqhaydar@gmail.com>
2026-09-09 05:38:57 -0700

    Implement initial IG Local profile UI

--- app/src/main/java/com/example/iglocal/MainActivity.kt
+++ app/src/main/java/com/example/iglocal/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.iglocal
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +12,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val imgProfile = findViewById<ImageView>(R.id.imgProfile)
+        imgProfile.clipToOutline = true
     }
 }
